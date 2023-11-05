d050224677243d5b75491d45638e06af95787278
Abhav Goel <goelabhav2002@gmail.com>
2023-11-06 01:20:43 +0530

    database connection

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,14 @@
 from flask import Flask
+from .config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SECRET_KEY']= 'you-will-never-guess'
 
-from app import routes
+app.config.from_object(Config)#configuaration settings from class Config
+
+db=SQLAlchemy(app)# setting up SQLalchemy db interface to interact with db
+
+migrate = Migrate(app,db)
+
+from app import routes,models
